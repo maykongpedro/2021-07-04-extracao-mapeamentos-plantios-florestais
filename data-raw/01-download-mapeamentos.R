91a5a196f4343c1cdd6e290ad89ee366e2a2ce12
maykongpedro <maykonglaffite@gmail.com>
2021-07-04 23:57:15 -0300

    add script de download dos pdfs

---
+++ data-raw/01-download-mapeamentos.R
@@ -0,0 +1,11 @@
+    
+
+
+# Funficionou, porém vou ter um trabalho desnecessário baixando pdf por pdf
+url <- "https://drive.google.com/uc?export=download&id=1stOUQ7Cmyhu8gMnVdoNo2qswgyRoDj6f"
+download.file(url, destfile = "./data-raw/pdf/01-Brasil/-RELATORIO-IBA-2020.pdf")
+
+
+# Nao funcionou
+url_zip <- "https://drive.google.com/uc?export=download&id=1Uc-O3gM3duG_obfzeEzIaACSKgy6CF63"
+download.file(url, destifile = temp)
