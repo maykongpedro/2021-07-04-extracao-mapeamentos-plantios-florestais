b077885a747646158b5ff2348c6a3fb4e7597b34
maykongpedro <maykonglaffite@gmail.com>
2021-07-12 23:42:00 -0300

    add inicío do script do ibge

---
+++ data-raw/03-ext-fax-mapeamento-ibge.R
@@ -0,0 +1,16 @@
+
+# Carregar e instalar pacotes ---------------------------------------------
+
+# if(!require("pacman")) install.packages("pacman")
+# pacman::p_load(tidyverse, readr, tabulizer, purrr, janitor)
+
+# Carregar pipe
+'%>%' <- magrittr::`%>%`
+
+
+# Importar csv ------------------------------------------------------------
+
+
+
+
+# Importar pdfs -----------------------------------------------------------
