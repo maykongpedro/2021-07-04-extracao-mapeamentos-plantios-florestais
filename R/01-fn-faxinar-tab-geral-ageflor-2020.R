5b31c1c2ff8912b530144a8ee2eed8eeddc22aa1
maykongpedro <maykonglaffite@gmail.com>
2021-07-17 16:24:17 -0300

    add funções para organizar tabelas da AGEFLOR 2020

---
+++ R/01-fn-faxinar-tab-geral-ageflor-2020.R
@@ -0,0 +1,22 @@
+#' faxinar_ageflor_2020_geral
+#'
+#' Descrição da função
+#' Função utilizada para faxinar as tabelas gerais (tabela 1 e 2) do mapeamento
+#' da AGEFLOR - 2020.
+#' 
+#' @param tabelas_extraidas tabela extraida do pdf pela função tabulizer::extract_tables
+#' @param nome_tabela nome do tabela que será faxinada
+#' 
+#' @return retorna uma tibble organizada e faxinada com as informações das tabelas
+
+faxinar_ageflor_2020_geral <-function(tabelas_extraidas, nome_tabela){
+    
+    tabelas_extraidas %>% 
+        purrr::pluck(nome_tabela) %>% 
+        tibble::as.tibble(.name_repair = "unique") %>%
+        janitor::row_to_names(1) %>%
+        janitor::clean_names() %>%
+        dplyr::mutate(dplyr::across(.cols = 2:5,
+                                    readr::parse_number, locale = loc))
+    
+}
---
+++ R/02-fn-faxinar-tab-municipios-ageflor-2020.R
@@ -0,0 +1,45 @@
+#' faxinar_ageflor_2020_muni
+#'
+#' Descrição da função
+#' Função utilizada para faxinar as tabelas de municípios (tabeles 4,6, 7 e 8) 
+#' do mapeamento da AGEFLOR - 2020.
+#' 
+#' @param tabelas_extraidas tabela extraida do pdf pela função tabulizer::extract_tables
+#' @param nome_tabela nome do tabela que será faxinada
+#' 
+#' @return retorna uma tibble organizada e faxinada com as informações das tabelas
+
+faxinar_ageflor_2020_muni <-function(tabelas_extraidas, nome_tabela){
+    
+    tab_completa <- tabelas_extraidas %>% 
+        purrr::pluck(nome_tabela) %>%
+        tibble::as.tibble(.name_repair = "unique") %>%
+        janitor::row_to_names(1) %>%
+        janitor::clean_names() %>% 
+        dplyr::select(-x,- x_2) %>% 
+        dplyr::filter(municipio != "Total") %>%
+        dplyr::mutate(
+            dplyr::across(.cols = c("area_ha", "area_ha_2"),
+                          readr::parse_number, locale = loc)
+        )
+    
+    # separar as colunas de municípios entre diferentes bases
+    muni_1 <- tab_completa %>% 
+        dplyr::select(municipio, area_ha)
+    
+    muni_2 <- tab_completa %>% 
+        dplyr::select(municipio_cont, area_ha_2) %>% 
+        dplyr::rename(municipio = "municipio_cont",
+                      area_ha = "area_ha_2")
+    
+    # empilhar as duas bases de muni
+    tab_final <- dplyr::bind_rows(muni_1, muni_2)
+    
+    # adicionar a info da tabela
+    tab_final %>% 
+        dplyr::mutate(
+            genero = nome_tabela,
+            genero = stringr::str_remove_all(genero, "muni_")
+            )
+    
+}
