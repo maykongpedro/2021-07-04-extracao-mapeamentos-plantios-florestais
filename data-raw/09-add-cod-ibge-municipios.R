2462ab8825f346fd96e13563803f421180533b83
maykongpedro <maykonglaffite@gmail.com>
2021-07-19 22:08:47 -0300

    corrigindo sintaxe de importação

--- data-raw/09-add-cod-ibge-municipios.R
+++ data-raw/09-add-cod-ibge-municipios.R
@@ -9,16 +9,10 @@
 
 # Importando bases organizadas --------------------------------------------
 
-# Lista de arquivos de municípios
-arquivos <- list.files("./data/", full.names = TRUE, pattern = "MUNICIPIOS")
-
-# Retirar da lista o mapeamento do IFPR (pois já tem código do IBGE)
-arquivos <- arquivos[-2]
-
-# Abrindo arquivos
-mapeamentos_municipios <- arquivos %>%
-    purrr::map(.f = readr::read_rds)
-
+base_ibge <- readr::read_rds("./data/BR_IBGE_SNIF_HISTORICO_MUNICIPIOS_2014_2016.RDS")
+base_famato_2013 <- readr::read_rds("data/MT_FAMATO_MUNICIPIOS_2013.RDS")
+base_ageflor_2017 <- readr::read_rds("data/RS_AGEFLOR_MUNICIPIOS_2017.RDS")
+base_ageflor_2020 <- readr::read_rds("data/RS_AGEFLOR_MUNICIPIOS_2020.RDS")
 
 
 # Importando base do IBGE -------------------------------------------------
@@ -32,6 +26,21 @@ base_muni_rs_mg <- base_muni %>%
     dplyr::filter(uf %in% c("RS", "MT")) 
 
 base_muni_rs_mg
+
+
+
+# Adicionando código município - Mapeamento IBGE - 2014 até 2016 ----------
+# Fazer join
+famato_2013 <- mapeamentos_municipios %>% 
+    purrr::pluck(1) %>% 
+    dplyr::left_join(base_muni_rs_mg) %>% 
+    dplyr::select(-code_state) %>% 
+    dplyr::relocate(code_muni, .before = "genero")
+
+# Verificar itens não encontrados
+famato_2013 %>% 
+    dplyr::filter(is.na(code_muni)) 
+
 
 
 # Adicionando código município - Mapeamento FAMATO - 2013  ---------------
@@ -85,7 +94,6 @@ ageflor_2017 %>%
 
 
 # Salvar bases ------------------------------------------------------------
-
 
 famato_2013 %>% saveRDS("./data/MT_FAMATO_MUNICIPIOS_2013_COD-IBGE.RDS")
 ageflor_2017 %>% saveRDS("./data/RS_AGEFLOR_MUNICIPIOS_2017_COD-IBGE.RDS")
